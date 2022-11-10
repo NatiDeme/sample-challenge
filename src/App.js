8eea38b55322f70f4abb8b19f4adf5affb900027
NatiDeme <demelashnatnael@gmail.com>
2022-11-11 02:10:25 +0300

    Add fix to router setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/sidenav/sidenav';
 import Tour from './components/pages/tour';
 import Reservation from './components/pages/reservation';
+import Reserve from './components/pages/reserve';
 
 function App() {
   return (
     <Router>
       <div className="flex">
-        <div className="w-64 border-x-2 border-black h-screen">
+        <div className="w-52 border-x-2 border-black h-screen">
           <Nav />
         </div>
-        <div>
-          <Routes>
-            <Route path="/" element={<Tour />} />
-            <Route path="/reserve" element={<Reservation />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Tour />} />
+          <Route path="/my-reservations" element={<Reservation />} />
+          <Route path="/reserve" element={<Reserve />} />
+        </Routes>
       </div>
     </Router>
   );
--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -1,10 +1,20 @@
-import Menu from './menu';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <div>
-      <Menu />
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/"> Tour </Link>
+        </li>
+        <li>
+          <Link to="/my-reservations"> My Reservation </Link>
+        </li>
+        <li>
+          <Link to="/reserve"> Reserve </Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
