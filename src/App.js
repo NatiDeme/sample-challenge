4587825aead3eaec0970dfce144f90079bb48ce1
NatiDeme <demelashnatnael@gmail.com>
2022-11-14 12:02:53 +0300

    Add respondive navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Nav from './components/sidenav/sidenav';
-// import Tour from './components/pages/tour';
-// import Reservation from './components/pages/reservation';
-// import Reserve from './components/pages/reserve';
-// import CreateTour from './components/pages/new_tour';
-// import DeleteTour from './components/pages/delete_tour';
-// import Login from './components/pages/login';
-import Signup from './components/pages/signup';
+import Nav from './components/sidenav/sidenav';
+import Tour from './components/pages/tour';
+import Reservation from './components/pages/reservation';
+import Reserve from './components/pages/reserve';
+import CreateTour from './components/pages/new_tour';
+import DeleteTour from './components/pages/delete_tour';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-      </Routes>
-      {/* <div className="flex">
-        <div className="border-x-2 h-screen w-72 drop-shadow-sm">
+      <div className="flex">
+        <div className="border-x-2 h-screen md:w-72 w-16 drop-shadow-sm">
           <Nav />
         </div>
         <Routes>
@@ -25,7 +20,7 @@ function App() {
           <Route path="/create-tour" element={<CreateTour />} />
           <Route path="/delete-tour" element={<DeleteTour />} />
         </Routes>
-      </div> */}
+      </div>
     </Router>
   );
 }
--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <>
-      <ul className="h-screen px-5 flex flex-col gap-3 pt-10 text-xl font-bold tracking-wide">
+      <p className="text-bold font-black pt-5 pl-2 md:pl-20">LOGO</p>
+      <ul className="h-screen px-5 md:flex flex-col gap-3 mt-16 text-xl font-bold tracking-wide hidden">
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
           <Link to="/"> TOUR </Link>
         </li>
@@ -18,6 +19,23 @@ function Nav() {
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
           <Link to="/delete-tour"> DELETE TOUR </Link>
+        </li>
+      </ul>
+      <ul className="h-screen px-3 flex flex-col gap-3 pt-10 text-xl font-bold tracking-wide">
+        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
+          <Link to="/"> T</Link>
+        </li>
+        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
+          <Link to="/my-reservations"> M-R </Link>
+        </li>
+        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
+          <Link to="/reserve"> R-T</Link>
+        </li>
+        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
+          <Link to="/create-tour"> C-T</Link>
+        </li>
+        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
+          <Link to="/delete-tour"> D-T </Link>
         </li>
       </ul>
     </>
