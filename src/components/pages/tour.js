c7e71918d898d239c3da0081a03f3d7d7364a13a
NatiDeme <demelashnatnael@gmail.com>
2022-11-17 16:21:34 +0300

    Add sliding pagination

--- src/components/pages/tour.js
+++ src/components/pages/tour.js
@@ -5,20 +5,37 @@ function Tour() {
         <p className="text-3xl font-black">Latest Tours</p>
         <p>Please select a Tour</p>
       </div>
-      <div className="bg-gray-200 w-14 p-5 rounded-r-full bg-opacity-50 hidden lg:block">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 text-white">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
-          />
-        </svg>
+      <div className="flex justify-between">
+        <div className="bg-gray-200 w-14 p-5 rounded-r-full bg-opacity-50 hidden lg:block">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-white">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            />
+          </svg>
+        </div>
+        <div className="bg-black w-14 p-5 rounded-l-full hidden lg:block">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-white">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
