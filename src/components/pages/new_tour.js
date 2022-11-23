76e239cec72bf67811bbcc61a8efa700010519c2
NatiDeme <demelashnatnael@gmail.com>
2022-11-23 12:02:33 +0300

    Add form to new tour page

--- src/components/pages/new_tour.js
+++ src/components/pages/new_tour.js
@@ -1,3 +1,51 @@
 export default function CreateTour() {
-  return <div>new_tour</div>;
+  return (
+    <div className="w-full">
+      <div className="text-center mt-5">
+        <p className="text-3xl font-black">Create new Tour</p>
+      </div>
+      <div className="mt-10 w-full">
+        <form className="flex flex-col gap-5 md:mx-80">
+          <input
+            type="text"
+            className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
+            placeholder="Title"
+          />
+          <div className="flex gap-5">
+            <div className="flex flex-col gap-5">
+              <input
+                type="number"
+                className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
+                id="date"
+                placeholder="Duration"
+              />
+              <input
+                type="text"
+                className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
+                placeholder="City"
+              />
+            </div>
+            <div className="flex flex-col gap-5">
+              <input
+                type="number"
+                className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
+                id="date"
+                placeholder="Cost"
+              />
+              <input
+                className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
+                id="date"
+                placeholder="Photo URL"
+              />
+            </div>
+          </div>
+          <textarea
+            className="bg-slate-100 rounded-md focus:outline-2 focus:outline-slate-500"
+            id="date"
+            placeholder="Description"
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
