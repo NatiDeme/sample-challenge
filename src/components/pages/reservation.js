70542e8f25f94085c2117b59d2af4494699456b0
NatiDeme <demelashnatnael@gmail.com>
2022-11-26 04:12:43 +0300

    Add style and card to reservations component

--- src/components/pages/reservation.js
+++ src/components/pages/reservation.js
@@ -1,5 +1,44 @@
 function Reservation() {
-  return <div>this is reservation form</div>;
+  return (
+    <div className="w-full">
+      <div className=" flex flex-col items-center pt-5">
+        <p className="text-3xl font-black">Your Tours</p>
+      </div>
+      <div className=" flex-col items-center pt-5 hidden">
+        <p className="text-3xl font-black">You have not created any reservations yet</p>
+        <p>Please select a Tour</p>
+      </div>
+      <div className="flex gap-8 border-2 border-slate-100 w-[35rem] rounded-xl p-3">
+        <div>
+          <img
+            src="https://images.pexels.com/photos/158398/niagara-falls-waterfall-horseshoe-158398.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt="me"
+            className="rounded-lg h-80 w-[60rem]"
+          />
+        </div>
+
+        <div>
+          <p className="text-xl font-black">Visit Nirobi</p>
+          <p className="text-gray-500 mt-5">
+            Nairobi is Kenya’s capital city. In addition to its urban core, the city has Nairobi
+            National Park, a large game reserve known for breeding endangered black rhinos and home
+            to giraffes, zebras and lions.
+          </p>
+          <div className="flex gap-3 text-gray-500 mt-5">
+            <p className="">Reserved Date: </p>
+            <p className="">Jan, 2022 </p>
+          </div>
+          <div className="mt-3">
+            <button
+              type="button"
+              className="bg-slate-700 text-white p-3 rounded-xl hover:bg-red-500">
+              Cancel Reservation
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Reservation;
--- src/components/pages/reserve.js
+++ src/components/pages/reserve.js
@@ -6,14 +6,18 @@ export default function Reserve() {
       </div>
       <div className="flex justify-center mt-10">
         <form className="flex flex-col gap-5">
-          <input type="date" className="border-b-2 border-gray-600 p-1 focus:outline-0" id="date" />
+          <input
+            type="date"
+            className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
+            id="date"
+          />
           <input
             type="text"
-            className="border-b-2 border-gray-600 p-1 focus:outline-0"
+            className="border-b-2 border-gray-200 focus:border-slate-500  p-1 focus:outline-0"
             id="date"
             placeholder="Name"
           />
-          <select className="bg-white border-b-2 border-gray-600 p-1 focus:outline-0">
+          <select className="bg-slate-100 rounded-md focus:outline-2 focus:outline-slate-500 h-10">
             <option>Kenya</option>
             <option>Ethipia</option>
           </select>
