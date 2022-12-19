cefab8a6721619ee3ce1f9ca96fd92f732571017
NatiDeme <demelashnatnael@hotmail.com>
2022-12-19 20:15:37 +0300

    Add configuration and design to swiper implementation

--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -1,41 +1,47 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
     <>
       <p className="text-bold font-black pt-5 pl-2 md:pl-20">LOGO</p>
       <ul className="h-screen px-5 md:flex flex-col gap-3 mt-16 text-xl font-bold tracking-wide hidden">
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/"> TOUR </Link>
+        <li>
+          <NavLink
+            to="/"
+            className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2 active:bg-red"
+            activeClassName="selected">
+            {' '}
+            TOUR{' '}
+          </NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/my-reservations"> MY RESERVATIONS </Link>
+          <NavLink to="/my-reservations"> MY RESERVATIONS </NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/reserve"> RESERVE TOUR </Link>
+          <NavLink to="/reserve"> RESERVE TOUR </NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/create-tour"> CREATE TOUR </Link>
+          <NavLink to="/create-tour"> CREATE TOUR </NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/delete-tour"> DELETE TOUR </Link>
+          <NavLink to="/delete-tour"> DELETE TOUR </NavLink>
         </li>
       </ul>
       <ul className="h-screen px-3 flex flex-col gap-3 pt-10 text-xl font-bold tracking-wide lg:hidden">
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/"> T</Link>
+          <NavLink to="/"> T</NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/my-reservations"> M-R </Link>
+          <NavLink to="/my-reservations"> M-R </NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/reserve"> R-T</Link>
+          <NavLink to="/reserve"> R-T</NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/create-tour"> C-T</Link>
+          <NavLink to="/create-tour"> C-T</NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <Link to="/delete-tour"> D-T </Link>
+          <NavLink to="/delete-tour"> D-T </NavLink>
         </li>
       </ul>
     </>
