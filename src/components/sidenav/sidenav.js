fd6def604490ce75af7e18f941f668c333766c1a
NatiDeme <demelashnatnael@hotmail.com>
2022-12-20 16:24:28 +0300

    Add sign in button on navbr

--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -4,15 +4,9 @@ function Nav() {
   return (
     <>
       <p className="text-bold font-black pt-5 pl-2 md:pl-20">LOGO</p>
-      <ul className="h-screen px-5 md:flex flex-col gap-3 mt-16 text-xl font-bold tracking-wide hidden">
-        <li>
-          <NavLink
-            to="/"
-            className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2 active:bg-red"
-            activeClassName="selected">
-            {' '}
-            TOUR{' '}
-          </NavLink>
+      <ul className=" px-5 md:flex flex-col gap-3 mt-16 text-xl font-bold tracking-wide hidden">
+        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
+          <NavLink to="/"> TOUR </NavLink>
         </li>
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
           <NavLink to="/my-reservations"> MY RESERVATIONS </NavLink>
@@ -27,7 +21,7 @@ function Nav() {
           <NavLink to="/delete-tour"> DELETE TOUR </NavLink>
         </li>
       </ul>
-      <ul className="h-screen px-3 flex flex-col gap-3 pt-10 text-xl font-bold tracking-wide lg:hidden">
+      <ul className="px-3 flex flex-col gap-3 pt-10 text-xl font-bold tracking-wide lg:hidden">
         <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
           <NavLink to="/"> T</NavLink>
         </li>
@@ -44,6 +38,13 @@ function Nav() {
           <NavLink to="/delete-tour"> D-T </NavLink>
         </li>
       </ul>
+      <div className="flex justify-center mt-5">
+        <button
+          type="button"
+          className="bg-slate-700 text-white p-2 rounded-lg hover:bg-slate-500 px-10">
+          Sign In
+        </button>
+      </div>
     </>
   );
 }
