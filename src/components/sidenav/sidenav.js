f55dfc2ee60ec35a1137cef0848bab82d08351d6
NatiDeme <demelashnatnael@hotmail.com>
2022-12-27 17:21:23 +0300

    Add merge change on sidenav

--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -9,42 +9,6 @@ function Nav() {
   };
   return (
     <>
-<<<<<<< HEAD
-      <p className="text-bold font-black pt-5 pl-2 md:pl-20">LOGO</p>
-      <ul className=" px-5 md:flex flex-col gap-3 mt-16 text-xl font-bold tracking-wide hidden">
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/"> TOUR </NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/my-reservations"> MY RESERVATIONS </NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/reserve"> RESERVE TOUR </NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/create-tour"> CREATE TOUR </NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/delete-tour"> DELETE TOUR </NavLink>
-        </li>
-      </ul>
-      <ul className="px-3 flex flex-col gap-3 pt-10 text-xl font-bold tracking-wide lg:hidden">
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/"> T</NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/my-reservations"> M-R </NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/reserve"> R-T</NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/create-tour"> C-T</NavLink>
-        </li>
-        <li className="hover:bg-slate-700 hover:text-white active:bg-slate-700 p-2">
-          <NavLink to="/delete-tour"> D-T </NavLink>
-        </li>
-=======
       <ul className="hidden md:flex flex-col gap-3 mt-16 text-xl font-bold tracking-wide p-10">
         <NavLink to="/" className="">
           {' '}
@@ -136,7 +100,6 @@ function Nav() {
             D-T
           </li>
         </NavLink>
->>>>>>> edit-layout
       </ul>
       <div className="flex justify-center mt-5">
         <button
