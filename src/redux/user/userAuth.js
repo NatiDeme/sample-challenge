2887785785bd92210c6581e767480d4030cb4f7f
NatiDeme <demelashnatnael@hotmail.com>
2022-12-30 13:23:06 +0300

    Add minor change on userAuth

--- src/redux/user/userAuth.js
+++ src/redux/user/userAuth.js
@@ -1,12 +1,28 @@
 const LOGIN_USER = 'LOGIN_USER';
 const LOGIN = 'https://tour-booking.onrender.com/login';
-// const SIGNUP_USER = 'SIGNUP_USER';
+const SIGNUP_USER = 'SIGNUP_USER';
 const user = [];
 export const logInUser = (payload) => ({
   type: LOGIN_USER,
   payload
 });
+export const signUp = (payload) => ({
+  type: SIGNUP_USER,
+  payload
+});
 export const authUser = (data) => async (dispatch) => {
+  const res = await fetch(LOGIN, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+  if (res.status === 200) {
+    const final = await res.json();
+    dispatch(logInUser(final));
+  }
+};
+
+export const signUpUser = (data) => async (dispatch) => {
   const res = await fetch(LOGIN, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
